18eb64a94c7baf3ab800f2e54a5a4513be982964
ch.kuehnel <ch.kuehnel@gmx.de>
2014-08-02 13:35:53 +0200

    header and source file for accelerometer use added

---
+++ src/acc.c
@@ -0,0 +1,142 @@
+/*
+ * acc_init.c
+ *
+ *  Created on: 02.08.2014
+ *      Author: christian
+ */
+
+/***********************************
+ * Includes
+ ***********************************/
+#include "acc.h"
+#include "stm32f3_discovery_lsm303dlhc.h"
+
+/***********************************
+ * Defines
+ ***********************************/
+#define PI                         (float)     3.14159265f
+/*!< accelerometer sensitivity with 2 g full scale [LSB/mg] */
+#define LSM_Acc_Sensitivity_2g     (float)     1.0f
+/*!< accelerometer sensitivity with 4 g full scale [LSB/mg] */
+#define LSM_Acc_Sensitivity_4g     (float)     0.5f
+/*!< accelerometer sensitivity with 8 g full scale [LSB/mg] */
+#define LSM_Acc_Sensitivity_8g     (float)     0.25f
+/*!< accelerometer sensitivity with 12 g full scale [LSB/mg] */
+#define LSM_Acc_Sensitivity_16g    (float)     0.0834f
+
+/***********************************
+ * Functions
+ ***********************************/
+/**
+ * call accInit to initialize Accelerometer
+ */
+void accInit(){
+	LSM303DLHCAcc_InitTypeDef LSM303DLHCAcc_InitStructure;
+	LSM303DLHCAcc_FilterConfigTypeDef LSM303DLHCFilter_InitStructure;
+
+	/* Fill the accelerometer structure */
+	LSM303DLHCAcc_InitStructure.Power_Mode = LSM303DLHC_NORMAL_MODE;
+	LSM303DLHCAcc_InitStructure.AccOutput_DataRate = LSM303DLHC_ODR_50_HZ;
+	LSM303DLHCAcc_InitStructure.Axes_Enable= LSM303DLHC_AXES_ENABLE;
+	LSM303DLHCAcc_InitStructure.AccFull_Scale = LSM303DLHC_FULLSCALE_2G;
+	LSM303DLHCAcc_InitStructure.BlockData_Update = LSM303DLHC_BlockUpdate_Continous;
+	LSM303DLHCAcc_InitStructure.Endianness=LSM303DLHC_BLE_LSB;
+	LSM303DLHCAcc_InitStructure.High_Resolution=LSM303DLHC_HR_ENABLE;
+	/* Configure the accelerometer main parameters */
+	LSM303DLHC_AccInit(&LSM303DLHCAcc_InitStructure);
+
+	/* Fill the accelerometer LPF structure */
+	LSM303DLHCFilter_InitStructure.HighPassFilter_Mode_Selection =LSM303DLHC_HPM_NORMAL_MODE;
+	LSM303DLHCFilter_InitStructure.HighPassFilter_CutOff_Frequency = LSM303DLHC_HPFCF_16;
+	LSM303DLHCFilter_InitStructure.HighPassFilter_AOI1 = LSM303DLHC_HPF_AOI1_DISABLE;
+	LSM303DLHCFilter_InitStructure.HighPassFilter_AOI2 = LSM303DLHC_HPF_AOI2_DISABLE;
+
+	/* Configure the accelerometer LPF main parameters */
+	LSM303DLHC_AccFilterConfig(&LSM303DLHCFilter_InitStructure);
+}
+
+/**
+* @brief Read LSM303DLHC output register, and calculate the acceleration ACC=(1/SENSITIVITY)*
+* @param pnData: pointer to float buffer where to store data
+* @retval None
+*/
+void accReadData(float* pResultData)
+{
+  int16_t pnRawData[3];
+  uint8_t ctrlx[2];
+  float LSM_Acc_Sensitivity = LSM_Acc_Sensitivity_2g;
+  uint8_t buffer[6], cDivider;
+  uint8_t i = 0;
+
+  /* Read the register content */
+  LSM303DLHC_Read(ACC_I2C_ADDRESS, LSM303DLHC_CTRL_REG4_A, ctrlx,2);
+  LSM303DLHC_Read(ACC_I2C_ADDRESS, LSM303DLHC_OUT_X_L_A, buffer, 6);
+
+
+  if(ctrlx[1]&0x40)
+    cDivider=64;
+  else
+    cDivider=16;
+
+  /* check in the control register4 the data alignment*/
+  if(!(ctrlx[0] & 0x40) || (ctrlx[1] & 0x40)) /* Little Endian Mode or FIFO mode */
+  {
+    for(i=0; i<3; i++)
+    {
+      pnRawData[i]=((int16_t)((uint16_t)buffer[2*i+1] << 8) + buffer[2*i])/cDivider;
+    }
+  }
+  else /* Big Endian Mode */
+  {
+    for(i=0; i<3; i++)
+      pnRawData[i]=((int16_t)((uint16_t)buffer[2*i] << 8) + buffer[2*i+1])/cDivider;
+  }
+  /* Read the register content */
+  LSM303DLHC_Read(ACC_I2C_ADDRESS, LSM303DLHC_CTRL_REG4_A, ctrlx,2);
+
+
+  if(ctrlx[1]&0x40)
+  {
+    /* FIFO mode */
+    LSM_Acc_Sensitivity = 0.25;
+  }
+  else
+  {
+    /* normal mode */
+    /* switch the sensitivity value set in the CRTL4*/
+    switch(ctrlx[0] & 0x30)
+    {
+    case LSM303DLHC_FULLSCALE_2G:
+      LSM_Acc_Sensitivity = LSM_Acc_Sensitivity_2g;
+      break;
+    case LSM303DLHC_FULLSCALE_4G:
+      LSM_Acc_Sensitivity = LSM_Acc_Sensitivity_4g;
+      break;
+    case LSM303DLHC_FULLSCALE_8G:
+      LSM_Acc_Sensitivity = LSM_Acc_Sensitivity_8g;
+      break;
+    case LSM303DLHC_FULLSCALE_16G:
+      LSM_Acc_Sensitivity = LSM_Acc_Sensitivity_16g;
+      break;
+    }
+  }
+
+  /* Obtain the mg value for the three axis */
+  for(i=0; i<3; i++)
+  {
+	  pResultData[i]=(float)pnRawData[i]/LSM_Acc_Sensitivity;
+  }
+}
+
+/**
+ * Callback for timeout
+ */
+uint32_t LSM303DLHC_TIMEOUT_UserCallback(void)
+{
+  /* Block communication and all processes */
+  while (1)
+  {
+  }
+}
+
+
---
+++ src/acc.h
@@ -0,0 +1,15 @@
+/*
+ * acc_init.h
+ *
+ *  Created on: 02.08.2014
+ *      Author: christian
+ */
+
+#ifndef ACC_INIT_H_
+#define ACC_INIT_H_
+
+
+void accInit();
+void accReadData(float *pResultData);
+
+#endif /* ACC_INIT_H_ */
